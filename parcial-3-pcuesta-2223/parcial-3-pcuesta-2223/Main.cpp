35ac2e09d18fe29e889e83b5ca5db12d24a95755
pcuestamorer <pablocuestam@outlook.com>
2023-01-17 23:03:21 +0100

    ini: parcial 3
    Delivery for  exam parcial 3 plantillas y excepciones 2022/2023

---
+++ parcial-3-pcuesta-2223/parcial-3-pcuesta-2223/Main.cpp
@@ -0,0 +1,9 @@
+// parcial-3-pcuesta-2223.cpp : This file contains the 'main' function. Program execution begins and ends there.
+
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    return 0;
+}
